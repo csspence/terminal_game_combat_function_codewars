41e77dec58e91f2f9f36dab28d5b28cefe9e71e7
Crew Spence <csspence123@gmail.com>
2020-09-06 05:08:36 -0500

    Wrote working solution.

--- terminal_game_combat_function_codewars.js
+++ terminal_game_combat_function_codewars.js
@@ -2,6 +2,6 @@
 Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
 */
 
-function combat(health, damage) {
-  // Write your code here
+const combat = (health, damage) => {
+  return health - damage > -1 ? health - damage : 0;
 }
